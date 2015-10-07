cc691431af042c5bbb48e85a16af0eceab1389ce
Tali Walt <taliwalt@gmail.com>
2015-10-07 01:53:25 -0700

    updated database config

--- config/database.js
+++ config/database.js
@@ -1,6 +1,5 @@
 // config/database.js
 module.exports = {
+  'url': 'mongodb://localhost:27017/fogo' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
-    'url' : 'mongodb://localhost/passport' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-
-};
+}
